05133150499b7d8d56d7bd4126faca60e2510599
Beatriz Huberto <169067859+trizrto@users.noreply.github.com>
2024-09-09 10:47:55 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int numero = sc.nextInt();
+      if (numero > 0){
+        System.out.println("ele é positivo");
+      
+      }
+    else{
+      System.out.println("ele é negativo");
+    }
+    
+
+
+
+
+
+    sc.close();  
+    
+    
+  }
+
+  
+}
